7d915491e1a663e8ff3acc1eb971e32a9e063dc3
Rim-9961 <3035194688@qq.com>
2025-01-25 19:06:47 +0800

    Site updated: 2025-01-25 19:06:46

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1737802772215";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1737803203548";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
