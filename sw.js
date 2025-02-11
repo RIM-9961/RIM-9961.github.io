8e6b82422df3a6bc8b16c7c64edb27f1ed687b0d
RIM <2492474702@qq.com>
2025-02-11 16:05:19 +0800

    Site updated: 2025-02-11 16:05:19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1738063590881";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1739261117173";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
