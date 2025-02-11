c933a4bd3b121e3b0d13a965a9d2770aa6bc5e8f
RIM <2492474702@qq.com>
2025-02-11 17:01:48 +0800

    Site updated: 2025-02-11 17:01:48

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1739264423994";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1739264506050";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
